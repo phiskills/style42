ef58460d53290f160ae2848fb4fd0a1dab7e047e
michele <rexha.michele@gmail.com>
2019-10-01 17:18:40 +0200

    add styled components and grommet

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,24 @@
  */
 
 // You can delete this file if you're not using it
+
+const cloudinary = require('cloudinary').v2
+
+require('dotenv').config({
+  path: `env.${process.env.NODE_ENV}`,
+});
+
+const current_batch = 'batchResult1'
+
+console.log(process.env)
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+cloudinary.search
+  .expression(`resource_type:image AND tags=${current_batch}`)
+  .with_field('tags')
+  .max_results(5)
+  .execute().then((result, error)=>console.log(result))
