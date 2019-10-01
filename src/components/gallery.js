7476c5e99a843ea9f3d5e95517f3bc22709b88fa
Michele Rexha <rexha.michele@gmail.com>
2019-10-01 18:48:31 +0200

    prepare for lambda function

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -4,14 +4,15 @@ import cloudinary from 'cloudinary-core';
 import Picture from "./picture"
 
 // 1. set up empty state
-// 2. call external cloudinary api to find images with tag='batchResult1'
-// 3. return list of elements
+// have a lambda function update a json file with pictures every hour or so
+// use pictures list inside this component to render
+// move cloudinary code to lambda function
 function Gallery(props) {
 
   const [pictures, setPictures] = useState([])
 
   useEffect(() => {
-    const cl = new cloudinary.Cloudinary({cloud_name: "style42", api_key:'934498994849366', api_secret:'7fsFdxrIzYlMdA7fuIHqXS1-WeI' });
+    const cl = new cloudinary.Cloudinary({cloud_name: "style42", api_key:'934498994849366', api_secret:'7fsFdxrIzYlMdA7fuIHqXS1-WeI', secure:true });
     const url = cl.url('batchResult1', {format: 'json', type: 'list'});
     console.log("url", JSON.parse(url))
 
