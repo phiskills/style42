a98031d718fd2c4b3f9428b9a8224620be448923
Michele Rexha <rexha.michele@gmail.com>
2019-10-09 15:19:35 +0200

    remove yaml

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        // icon: `pug-icon-big.png`, // This path is relative to the root of the site.
+        icon: `./src/images/pug-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,35 +1,35 @@
 
-exports.createPages = async ({ graphql, actions: { createPage } }) => {
-
-  return graphql(`
-          query {
-            allCloudinaryAsset {
-              tags
-              secure_url
-              public_id
-              id
-            }
-          }
-  `, { limit: 120 }).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
-
-    // createPage({
-    //   path: `/`,
-    //   component: require.resolve("./src/components/index.js"),
-    //   context: { pictures, logo: result.data.logo },
-    //   // // Create a page for each Picture.
-    //
-    // })
-
-    //todo
-  //   pictures.forEach(picture => {
-  //     createPage({
-  //       path: `/gallery/${picture.id}/`,
-  //       component: require.resolve("./src/components/tableau.js"),
-  //       context: { picture },
-  //     })
-  //   })
-  })
-}
+// exports.createPages = async ({ graphql, actions: { createPage } }) => {
+//   //
+//   // return graphql(`
+//   //         query {
+//   //           allCloudinaryAsset {
+//   //             tags
+//   //             secure_url
+//   //             public_id
+//   //             id
+//   //           }
+//   //         }
+//   // `, { limit: 120 }).then(result => {
+//   //   if (result.errors) {
+//   //     throw result.errors
+//   //   }
+//
+//     // createPage({
+//     //   path: `/`,
+//     //   component: require.resolve("./src/components/index.js"),
+//     //   context: { pictures, logo: result.data.logo },
+//     //   // // Create a page for each Picture.
+//     //
+//     // })
+//
+//     //todo
+//   //   pictures.forEach(picture => {
+//   //     createPage({
+//   //       path: `/gallery/${picture.id}/`,
+//   //       component: require.resolve("./src/components/tableau.js"),
+//   //       context: { picture },
+//   //     })
+//   //   })
+//   })
+// }
